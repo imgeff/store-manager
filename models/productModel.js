0077373709f9aed7fbbffae104544c68ba859c19
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-30 17:57:31 -0300

    feat: Implementa TDD na função getAll de ProductsModel

--- models/productModel.js
+++ models/productModel.js
@@ -1,1 +1,12 @@
-// Renomeie esse arquivo
+const connection = require('./connection');
+
+const getAll = async () => {
+  const queryGetAll = 'SELECT * FROM StoreManager.products;';
+  const [allProducts] = await connection.execute(queryGetAll);
+
+  return allProducts;
+};
+
+module.exports = {
+  getAll,
+};
--- test/unit/models/productModel.js
+++ test/unit/models/productModel.js
@@ -1,1 +1,32 @@
-// Renomeie esse arquivo
+const sinon = require('sinon');
+const { expect } = require('chai');
+const connection = require('../../../models/connection');
+const productModel = require('../../../models/productModel');
+
+const fakeAllProducts = [
+  {
+    id: 1,
+    name: "produto A",
+    quantity: 10
+  },
+  {
+    id: 2,
+    name: "produto B",
+    quantity: 20
+  }
+];
+
+describe('Função getAll em MODELS', () => {
+  before(() => {
+    sinon.stub(connection, 'execute').resolves([fakeAllProducts]);
+  })
+
+  after(() => {
+    connection.execute.restore();
+  })
+
+  it('Retorna lista de todos os produtos', async () => {
+    const resultgetAll = await productModel.getAll();
+    expect(resultgetAll).to.be.equal(fakeAllProducts);
+  })
+})
