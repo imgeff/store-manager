d2c1183f8ce2fb5fd98c5f141df441c9b07b3b1d
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-31 18:27:01 -0300

    feat: Implementa middleware saleValidation

--- middlewares/saleValidation.js
+++ middlewares/saleValidation.js
@@ -1,30 +1,29 @@
 const Joi = require('joi');
 
 const requiredValidate = Joi.object({
-  name: Joi.string().required(),
+  productId: Joi.number().required(),
   quantity: Joi.number().required(),
 });
 
 const greatestValidate = Joi.object({
-  name: Joi.string().min(5),
   quantity: Joi.number().min(1),
 });
 
-const productSchema = (fields) => {
+const saleSchema = (fields) => {
   const { error: errRequired } = requiredValidate.validate(fields);
-  const { error: errGreatest } = greatestValidate.validate(fields);
+  const { error: errGreatest } = greatestValidate.validate({ quantity: fields.quantity });
   if (errRequired) return { code: 400, content: errRequired.message };
   if (errGreatest) return { code: 422, content: errGreatest.message };
   return false;
 };
 
-const productValidation = (req, res, next) => {
+const saleValidation = (req, res, next) => {
   const { name, quantity } = req.body;
-  const resultValidation = productSchema({ name, quantity });
+  const resultValidation = saleSchema({ name, quantity });
   if (resultValidation !== false) {
     return res.status(resultValidation.code).json(resultValidation.content);
   }
   next();
 };
 
-module.exports = productValidation;
+module.exports = saleValidation;
