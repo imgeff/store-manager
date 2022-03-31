c4b7b17a85707ef205bc9682e1962b5364c965a1
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-03-31 18:19:15 -0300

    feat: Implementa middleware productValidation

---
+++ middlewares/productValidation.js
@@ -0,0 +1,30 @@
+const Joi = require('joi');
+
+const requiredValidate = Joi.object({
+  name: Joi.string().required(),
+  quantity: Joi.number().min(1).required(),
+});
+
+const greatestValidate = Joi.object({
+  name: Joi.string().min(5),
+  quantity: Joi.number().min(1),
+});
+
+const productSchema = (fields) => {
+  const { error: errRequired } = requiredValidate.validate(fields);
+  const { error: errGreatest } = greatestValidate.validate(fields);
+  if (errRequired) return { code: 400, content: errRequired.message };
+  if (errGreatest) return { code: 422, content: errGreatest.message };
+  return false;
+};
+
+const productValidation = (req, res, next) => {
+  const { name, quantity } = req.body;
+  const resultValidation = productSchema({ name, quantity });
+  if (resultValidation !== false) {
+    return res.status(resultValidation.code).json(resultValidation.content);
+  }
+  next();
+};
+
+module.exports = productValidation;
