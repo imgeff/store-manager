9fc59ea959118af1c5d3a0c023563ef1481067c5
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-01 16:26:28 -0300

    feat: Implementa função update na camada services

--- services/productService.js
+++ services/productService.js
@@ -1,5 +1,5 @@
 const productModel = require('../models/productModel');
-const verifyDuplicates = require('../helpers/verifyDuplicates');
+const search = require('../helpers/search');
 const { notFound } = require('../data/errorMessage');
 
 const getAll = async () => {
@@ -14,14 +14,22 @@ const getById = async (id) => {
 };
 
 const create = async ({ name, quantity }) => {
-  const resultDuplicates = await verifyDuplicates.products(name);
-  if (resultDuplicates) return { code: 409, content: { message: 'Product already exists' } };
+  const resultSearch = await search.products(name);
+  if (resultSearch) return { code: 409, content: { message: 'Product already exists' } };
   const resultcreate = await productModel.create({ name, quantity });
   return { code: 201, content: resultcreate };
+};
+
+const update = async ({ id, name, quantity }) => {
+  const resultSearch = await search.products(false, id);
+  if (!resultSearch) return { code: 404, content: { message: 'Product not found' } };
+  const resultUpdate = await productModel.update({ id, name, quantity });
+  return { code: 200, content: resultUpdate };
 };
 
 module.exports = {
   getAll,
   getById,
   create,
+  update,
 };
