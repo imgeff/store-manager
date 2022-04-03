68c50827c34d0d9391a9ea1d350c1f71af0030e7
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-03 16:12:50 -0300

    feat: Implementa funções de calcúlos de produtos nas ações de vendas

--- models/saleModel.js
+++ models/saleModel.js
@@ -1,5 +1,7 @@
 const connection = require('./connection');
+const { calculate, subtract, sum } = require('./inventoryProducts');
 
+// ================================ GET ALL  ==================================
 const getAll = async () => {
   const queryGetAll = `
   SELECT SP.sale_id AS saleId, SP.product_id AS productId, SP.quantity, S.date
@@ -8,10 +10,10 @@ const getAll = async () => {
   ON S.id = SP.sale_id
   ORDER BY SP.sale_id, SP.product_id;`;
   const [allSales] = await connection.execute(queryGetAll);
-
   return allSales;
 };
 
+// ================================ GET BY ID  ==================================
 const getById = async (id) => {
   const queryGetById = `
   SELECT SP.product_id AS productId, SP.quantity, S.date
@@ -24,58 +26,47 @@ const getById = async (id) => {
   return productById;
 };
 
+// ================================ CREATE  ==================================
 const create = async (sales) => {
+  // =============== SUBTRACT QUANTITY PRODUCTS ==============
+    await subtract(sales);
   // =============== INSERT TABLE SALES ==============
-
-  const queryCreateSales = `
-    INSERT INTO StoreManager.sales(date) 
-    VALUES(NOW())`;
-  const [{ insertId }] = await connection.execute(queryCreateSales);
-
+    const queryCreateSales = `
+      INSERT INTO StoreManager.sales(date) 
+      VALUES(NOW())`;
+    const [{ insertId }] = await connection.execute(queryCreateSales);
   // =============== INSERT TABLE SALES_PRODUCTS ==============
-  
-  await sales.forEach(async ({ productId, quantity }) => {
-    const queryCreateSalesProducts = `
-    INSERT INTO StoreManager.sales_products(sale_id, product_id, quantity) 
-    VALUES(?, ?, ?)`;
-    await connection.execute(queryCreateSalesProducts, [insertId, productId, quantity]);
-  });
-
-  // =============== UPDATE QUANTITY TABLE PRODUCTS ==============
-  await sales.forEach(async ({ productId, quantity }) => {
-    const queryUpdateQuantity = `
-    UPDATE StoreManager.products 
-    SET quantity = quantity - ?
-    WHERE id = ?;`;
-    await connection.execute(queryUpdateQuantity, [quantity, productId]);
-  });
-
-  return { id: insertId, itemsSold: sales };
+    await sales.forEach(async ({ productId, quantity }) => {
+      const queryCreateSalesProducts = `
+      INSERT INTO StoreManager.sales_products(sale_id, product_id, quantity) 
+      VALUES(?, ?, ?)`;
+      await connection.execute(queryCreateSalesProducts, [insertId, productId, quantity]);
+    });
+    return { id: insertId, itemsSold: sales };
 };
 
+// ================================ UPDATE  ==================================
 const update = async ({ id, productId, quantity }) => {
-  // =============== UPDATE TABLE PRODUCTS ==============
-  const queryUpdateProduct = `
-  UPDATE StoreManager.products
-  SET quantity = ?
-  WHERE id = ?;`;
-  await connection.execute(queryUpdateProduct, [quantity, productId]);
-
-  // =============== UPDATE TABLE SALES PRODUCTS ==============
-  const queryUpdateSalesProducts = `
-  UPDATE StoreManager.sales_products
-  SET product_id = ?, quantity = ?
-  WHERE sale_id = ?;`;
-  await connection.execute(queryUpdateSalesProducts, [productId, quantity, id]);
-  
-  return { saleId: id, itemUpdated: [{ productId, quantity }] };
+  // =============== QUANTITY PRODUCTS ==============
+    await calculate(id, productId, quantity);
+  // =============== SALES PRODUCTS ==============
+    const queryUpdateSalesProducts = `
+    UPDATE StoreManager.sales_products
+    SET product_id = ?, quantity = ?
+    WHERE sale_id = ?;`;
+    await connection.execute(queryUpdateSalesProducts, [productId, quantity, id]);
+    return { saleId: id, itemUpdated: [{ productId, quantity }] };
 };
 
+// ================================ EXCLUDE  ================================== 
 const exclude = async (id) => {
-  const queryDelete = `
-  DELETE FROM StoreManager.sales
-  WHERE id = ?;`;
-  await connection.execute(queryDelete, [id]);
+  // =============== SUM QUANTITY PRODUCTS ==============
+    await sum(id);
+  // =============== DELETE SALE TABLE SALES ==============
+    const queryDelete = `
+    DELETE FROM StoreManager.sales
+    WHERE id = ?;`;
+    await connection.execute(queryDelete, [id]);
 };
 
 module.exports = {
