cb0fac5c3f756c42f80f8381b1fce8c8e4cca914
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-02 22:27:41 -0300

    refactor: Implementa trycatch na camada saleController

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -1,36 +1,61 @@
 const saleService = require('../services/saleService');
 
 const getAll = async (_req, res) => {
-  const resultgetAll = await saleService.getAll();
-  return res.status(200).json(resultgetAll);
+  try {   
+    const resultgetAll = await saleService.getAll();
+    return res.status(200).json(resultgetAll);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).end();
+  }
 };
 
 const getById = async (req, res) => {
-  const { id } = req.params;
-  const { code, content } = await saleService.getById(id);
-
-  return res.status(code).json(content);
+  try {
+    const { id } = req.params;
+    const { code, content } = await saleService.getById(id);
+  
+    return res.status(code).json(content);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).end();
+  }
 };
 
 const create = async (req, res) => {
-  const { body } = req;
-  const { code, content } = await saleService.create(body);
-  return res.status(code).json(content);
+  try {
+    const { body } = req;
+    const { code, content } = await saleService.create(body);
+    return res.status(code).json(content);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).end();
+  }
 };
 
 const update = async (req, res) => {
-  const { id } = req.params;
-  const [{ productId, quantity }] = req.body;
-  const { code, content } = await saleService.update({ id: Number(id), productId, quantity });
-
-  return res.status(code).json(content);
+  try {  
+    const { id } = req.params;
+    const [{ productId, quantity }] = req.body;
+    const { code, content } = await saleService.update({ id: Number(id), productId, quantity });
+  
+    return res.status(code).json(content);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).end();
+  }
 };
 
 const exclude = async (req, res) => {
-  const { id } = req.params;
-  const { code, content } = await saleService.exclude(Number(id));
+  try {  
+    const { id } = req.params;
+    const { code, content } = await saleService.exclude(Number(id));
 
-  return res.status(code).json(content);
+    return res.status(code).json(content);
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).end();
+  }
 };
 
 module.exports = {
