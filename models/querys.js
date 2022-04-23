12fe769cf886ca27874bf25f592381b96ebfab9f
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-22 23:23:38 -0300

    feat: Adiciona querys de productModel

--- models/querys.js
+++ models/querys.js
@@ -1,57 +1,79 @@
-const GetAll = `
+// =================== PRODUCTS =====================
+const GetAllProducts = 'SELECT * FROM products;';
+
+const GetByIdProduct = 'SELECT * FROM products WHERE id = ?;';
+
+const CreateProduct = 'INSERT INTO products(name, quantity) VALUES(?, ?)';
+
+const UpdateProduct = `
+UPDATE products
+SET name = ?, quantity = ?
+WHERE id = ?;`;
+
+const DeleteProduct = `
+DELETE FROM products
+WHERE id = ?;`;
+
+// =================== SALES =====================
+const GetAllSales = `
 SELECT SP.sale_id AS saleId, SP.product_id AS productId, SP.quantity, S.date
-FROM StoreManager.sales_products SP
-INNER JOIN StoreManager.sales S
+FROM sales_products SP
+INNER JOIN sales S
 ON S.id = SP.sale_id
 ORDER BY SP.sale_id, SP.product_id;`;
 
-const GetById = `
+const GetByIdSale = `
 SELECT SP.product_id AS productId, SP.quantity, S.date
-FROM StoreManager.sales_products SP
-INNER JOIN StoreManager.sales S
+FROM sales_products SP
+INNER JOIN sales S
 ON S.id = SP.sale_id
 WHERE SP.sale_id = ?
 ORDER BY SP.sale_id, SP.product_id;`;
 
 const CreateSales = `
-INSERT INTO StoreManager.sales(date) 
+INSERT INTO sales(date) 
 VALUES(NOW())`;
 
 const CreateSalesProducts = `
-INSERT INTO StoreManager.sales_products(sale_id, product_id, quantity) 
+INSERT INTO sales_products(sale_id, product_id, quantity) 
 VALUES(?, ?, ?)`;
 
 const UpdateSalesProducts = `
-UPDATE StoreManager.sales_products
+UPDATE sales_products
 SET product_id = ?, quantity = ?
 WHERE sale_id = ?;`;
 
-const Delete = `
-DELETE FROM StoreManager.sales
+const DeleteSale = `
+DELETE FROM sales
 WHERE id = ?;`;
 
 const UpdateQuantity = `
-UPDATE StoreManager.products 
+UPDATE products 
 SET quantity = quantity + ?
 WHERE id = ?;`;
 
 const SumQuantity = `
-  UPDATE StoreManager.products 
+  UPDATE products 
   SET quantity = quantity + ?
   WHERE id = ?;`;
 
 const SubtractQuantity = `
-UPDATE StoreManager.products 
+UPDATE products 
 SET quantity = quantity - ?
 WHERE id = ?;`;
 
 module.exports = {
-  GetAll,
-  GetById,
+  GetAllProducts,
+  GetByIdProduct,
+  CreateProduct,
+  UpdateProduct,
+  DeleteProduct,
+  GetAllSales,
+  GetByIdSale,
   CreateSales,
   CreateSalesProducts,
   UpdateSalesProducts,
-  Delete,
+  DeleteSale,
   UpdateQuantity,
   SumQuantity,
   SubtractQuantity,
