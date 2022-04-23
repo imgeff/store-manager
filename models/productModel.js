c8cba4ae6514cb27c07b4405c4a211d3f98c1654
imgeff <geffersonsilvaaraujo.gs@gmail.com>
2022-04-22 23:24:56 -0300

    refactor: Importa querys para utilizar nas funções que manipulam o banco

--- models/productModel.js
+++ models/productModel.js
@@ -1,41 +1,38 @@
 const connection = require('./connection');
+const { 
+  GetAllProducts, 
+  GetByIdProduct, 
+  CreateProduct, 
+  UpdateProduct,
+  DeleteProduct,
+} = require('./querys');
 
 const getAll = async () => {
-  const queryGetAll = 'SELECT * FROM StoreManager.products;';
-  const [allProducts] = await connection.execute(queryGetAll);
+  const [allProducts] = await connection.execute(GetAllProducts);
 
   return allProducts;
 };
 
 const getById = async (id) => {
-  const queryGetById = 'SELECT * FROM StoreManager.products WHERE id = ?;';
-  const [productById] = await connection.execute(queryGetById, [id]);
-
+  const [productById] = await connection.execute(GetByIdProduct, [id]);
+  
   return productById[0];
 };
 
 const create = async ({ name, quantity }) => {
-  const queryCreate = 'INSERT INTO StoreManager.products(name, quantity) VALUES(?, ?)';
-  const [{ insertId }] = await connection.execute(queryCreate, [name, quantity]);
+  const [{ insertId }] = await connection.execute(CreateProduct, [name, quantity]);
 
   return { id: insertId, name, quantity };
 };
 
 const update = async ({ id, name, quantity }) => {
-  const queryUpdate = `
-  UPDATE StoreManager.products
-  SET name = ?, quantity = ?
-  WHERE id = ?;`;
-  await connection.execute(queryUpdate, [name, quantity, id]);
+  await connection.execute(UpdateProduct, [name, quantity, id]);
   
   return { id, name, quantity };
 };
 
 const exclude = async (id) => {
-  const queryDelete = `
-  DELETE FROM StoreManager.products
-  WHERE id = ?;`;
-  await connection.execute(queryDelete, [id]);
+  await connection.execute(DeleteProduct, [id]);
 };
 
 module.exports = {
